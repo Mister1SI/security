9655cf78ddf6b8016430dcd3f4152afb5bbe726c
Mister1SI <wazzup2690@gmail.com>
2024-05-07 07:52:48 -0500

    Add basic hash function

--- hash.c
+++ hash.c
@@ -1,11 +1,14 @@
 
 unsigned hash(char* str) {
-    
+
     unsigned hash = 0, i = 0;
+	char x = 0;
     while (str[i]) {
-        hash += str[i];
+        x = str[i];
+		hash += x;
         i++;
     }
     
+	return hash;
 }
 
